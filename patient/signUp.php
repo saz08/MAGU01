40780e20ae2a0505efe7cce1ebba797b202a41df
Sara Reid <sara063@googlemail.com>
2019-02-07 14:29:52 +0000

    had to take out script link so it wouldnt endlessly loop

--- patient/signUp.php
+++ patient/signUp.php
@@ -83,7 +83,6 @@ if($loginOK) {
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="../js/script.js"></script>
 
     <link rel="stylesheet" type="text/css" href="../stylesheets/stylesheet.css">
 
